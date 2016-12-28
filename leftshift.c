7f145085812e1191779ed41ff0ba38ba16995a70
karthikeyan97 <skarthikeyan046@gmail.com>
2016-12-28 17:01:33 +0530

    Create leftshift.c

---
+++ leftshift.c
@@ -0,0 +1,21 @@
+	#include<stdio.h>
+  main()
+  {
+      int i=1,c,a,m;
+      printf("enter the number and times of right shift\n");
+      scanf("%d",&a);
+      scanf("%d",&m);
+      m=m%5;
+      m=5-m;
+      while(m!=0)
+      {
+        i=i*10;
+        m--;
+
+      }
+
+      c=a%i;
+      a=a/i;
+      a=c*(100000/i)+a;
+      printf("\nthe required number is %d ",a);
+}
