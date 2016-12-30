557b7cafeec2f145310df8f430733f8a564932ef
karthikeyan97 <skarthikeyan046@gmail.com>
2016-12-30 22:03:46 +0530

    Create matdyn.c

---
+++ matdyn.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+
+int m,n,k,i,j,**p;
+scanf("%d%d%d",&m,&n,&k);
+p=(int **)malloc(sizeof(int *)*m);
+for( i=0;i<m;i++)
+{p[i]=(int*)malloc(sizeof(int)*n);
+for( j=0;j<n;j++)
+p[i][j]=i+j==k?1:0;
+
+}
+for( i=0;i<m;i++)
+{
+
+for( j=0;j<n;j++)
+printf("%d",p[i][j]);
+printf("\n");
+}
+
+}
