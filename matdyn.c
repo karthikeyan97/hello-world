c8929ecb59e75ce578dc0957fbd0eeba60c79031
karthikeyan97 <skarthikeyan046@gmail.com>
2016-12-30 22:11:22 +0530

    Update matdyn.c

--- matdyn.c
+++ matdyn.c
@@ -3,21 +3,22 @@
 main()
 {
 
-int m,n,k,i,j,**p;
-scanf("%d%d%d",&m,&n,&k);
-p=(int **)malloc(sizeof(int *)*m);
-for( i=0;i<m;i++)
-{p[i]=(int*)malloc(sizeof(int)*n);
-for( j=0;j<n;j++)
-p[i][j]=i+j==k?1:0;
+    int m,n,k,i,j,**p;
+    scanf("%d%d%d",&m,&n,&k);
+    p=(int **)malloc(sizeof(int *)*m);
+    for( i=0;i<m;i++)
+      {
+      p[i]=(int*)malloc(sizeof(int)*n);
+      for( j=0;j<n;j++)
+        p[i][j]=i+j==k?1:0;
 
-}
-for( i=0;i<m;i++)
-{
+      }
+     for( i=0;i<m;i++)
+      {
 
-for( j=0;j<n;j++)
-printf("%d",p[i][j]);
-printf("\n");
-}
+        for( j=0;j<n;j++)
+          printf("%d",p[i][j]);
+        printf("\n");
+      }
 
 }
