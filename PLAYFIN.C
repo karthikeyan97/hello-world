3dfde3a245194c6074a35bed37beb0e6eeb09292
karthikeyan97 <skarthikeyan046@gmail.com>
2017-02-02 17:11:45 +0530

    Update PLAYFIN.C

--- PLAYFIN.C
+++ PLAYFIN.C
@@ -16,99 +16,114 @@ int main()
 	{
 		for(i=0;i<4;i++)
 		{
-		printf("\n");
-		for(j=0;j<16;j++)
-			printf("-");
-		printf("\n");
-		for(j=0;j<4;j++)
+			printf("\n");
+			for(j=0;j<16;j++)
+				printf("-");
+			printf("\n");
+			for(j=0;j<4;j++)
 				if(nptr==i&&nptc==j)
-				printf("   |");
+				{
+					printf("   |");
+				}
 				else
+				{
 					printf("%2d |",a[i][j]);
-
+				}
 		}
-	printf("enter the choice \n 1.L-left 2.R=right 3.U-up 4.D-down 5.Q-quit");
-	if(count<50)
-	{
-	randi=random(4);
-	if(randi==0)
-		choice='l';
-		else
-			if(randi==1)
-				choice='r';
-				else
-					if(randi==2)
-						choice='u';
+		printf("enter the choice \n 1.L-left 2.R=right 3.U-up 4.D-down 5.Q-quit");
+		if(count<50)
+		{
+			randi=random(4);
+			if(randi==0)
+			{
+				choice='l';
+			}
+			
+			else
+				if(randi==1)
+				{
+					choice='r';
+				}
+					else
+						if(randi==2)
+						{
+							choice='u';
+						}
 							else
 								if(randi==3)
+								{	
 									choice='d';
-	}
-	else
-	{
-	scanf("%c",&choice);
-	}
-	switch(choice)
-	{
-		case 'l':
+								}
+		}
+		else
+		{
+			scanf("%c",&choice);
+		}
+		switch(choice)
+		{
+			case 'l':
 
-			 if(nptc-1>=0)
-				{
-					temp=a[nptr][nptc];
-					a[nptr][nptc]=a[nptr][nptc-1];
-					a[nptr][nptc-1]=temp;
-					nptc--;
+				 if(nptc-1>=0)
+					{
+						temp=a[nptr][nptc];
+						a[nptr][nptc]=a[nptr][nptc-1];
+						a[nptr][nptc-1]=temp;
+						nptc--;
 
-				}
-			   break;
-		case 'r':
+					}
+				   break;
+			case 'r':
 
-			 if(nptc+1<4)
-				{
-					temp=a[nptr][nptc];
-					a[nptr][nptc]=a[nptr][nptc+1];
-					a[nptr][nptc+1]=temp;
-					nptc++;
+				 if(nptc+1<4)
+					{
+						temp=a[nptr][nptc];
+						a[nptr][nptc]=a[nptr][nptc+1];
+						a[nptr][nptc+1]=temp;
+						nptc++;
 
-				}
-			 break;
-		case 'u':
+					}
+				 break;
+			case 'u':
 
-			 if(nptr-1>=0)
-				{
-					temp=a[nptr][nptc];
-					a[nptr][nptc]=a[nptr-1][nptc];
-					a[nptr-1][nptc]=temp;
-					nptr--;
+				 if(nptr-1>=0)
+					{
+						temp=a[nptr][nptc];
+						a[nptr][nptc]=a[nptr-1][nptc];
+						a[nptr-1][nptc]=temp;
+						nptr--;
 
-				}
-			break;
-		case 'd':
+					}
+				break;
+			case 'd':
 
-			 if(nptr+1<4)
-				{
-					temp=a[nptr][nptc];
-					a[nptr][nptc]=a[nptr+1][nptc];
-					a[nptr+1][nptc]=temp;
-					nptr++;
-				}
+				 if(nptr+1<4)
+					{
+						temp=a[nptr][nptc];
+						a[nptr][nptc]=a[nptr+1][nptc];
+						a[nptr+1][nptc]=temp;
+						nptr++;
+					}
 
-			break;
-		default:
-			printf("invalid input");
-			break;
+				break;
+			default:
+				printf("invalid input");
+				break;
 
 
 	   }
 	   count++;
 	   for(i=0;i<4;i++)
 	   for(j=0;j<0;j++)
+	   {
 		if(a[i][j]==(i+1)*(j+1))
-			ecount++;
+		 {
+			   ecount++;
+		 }
 		else
-			{
+		{
 			ecount=0;
 			break;
-			}
+		}
 		if(ecount==16)
 		{
 		printf("you win");
